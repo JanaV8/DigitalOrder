b167295a2acdf8c9682ee8c7ec5d2bdc2baae218
JanaV8 <janaviciana@hotmail.com.ar>
2024-09-20 12:07:43 -0300

    Clase Administrador

---
+++ Administrador.py
@@ -0,0 +1,30 @@
+
+#Clase Administrador con sus variables
+class Administrador:
+    def __init__(self, nombre, contraseña):
+        self.nombre = nombre
+        self.contraseña = contraseña
+
+    #Funcion para añadir un plato al menu
+    def añadir_plato(self, plato):
+        pass
+    
+    #Funcion para modificar un plato
+    def modificar_plato(self, plato):
+        pass
+    
+    #Funcion para eliminar un plato
+    def eliminar_plato(self, plato):
+        pass
+
+    #Funcion para añadir un ingrediente
+    def añadir_ingrediente(self, ingredientes):
+        pass
+    
+    #Funcion para modificar un ingrediente
+    def modificar_ingrediente(self, ingredientes):
+        pass
+
+    #Funcion para eliminar un ingrediente
+    def eliminar_ingrediente(self, ingredientes):
+        pass
