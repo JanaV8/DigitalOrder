6aeedecb54617575867a7cc5368ee3ee86d83b9f
JanaV8 <95034415+JanaV8@users.noreply.github.com>
2024-09-26 21:58:02 -0300

    Add files via upload

--- Administrador.py
+++ Administrador.py
@@ -1,38 +1,42 @@
-import sqlite3
-conn = sqlite3.connect("AdministradorBD")
+import pymysql
+
+conn = pymysql.connect( 
+        host='26.92.40.13',
+        user='root',
+        password='',
+        database='administrador_bd')
 cursor = conn.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS administradores (
-    id INTEGER PRIMARY KEY,
     usuario TEXT NOT NULL UNIQUE,
-    contraseña TEXT NOT NULL 
+    contraseña INTEGER PRIMARY KEY 
 )
 ''')
 
 #Clase Administrador con sus variables
 class Administrador:
     def __init__(self, usuario, contraseña):
-        self.usuariio = usuario
+        self.usuario = usuario
         self.contraseña = contraseña
 
     #Agregamos a un administrador a la base de datos, se necesita un usuario y una contrasena.
 def agregarAdministrador(usuario,contraseña):
     try:
         
-        cursor.execute('INSERT INTO administradores(usuario,contraseña) VALUES (?,?)', (usuario,contraseña))
+        cursor.execute('INSERT INTO administradores(usuario, contraseña) VALUES (%s, %s)', (usuario, contraseña))
         conn.commit()
         
         #Retornamos un mensaje de exito o de ya existencia dependiendo del caso
-        return 'Se agrego existosamente el usuario.'
-    except sqlite3.IntegrityError:
-        return 'El usuario ya existe.'
+        return 'Usuario Agregado Correctamente'
+    except pymysql.IntegrityError:
+        return 'Usuario existente'
     
 #Eliminamos un administrador de la base de dato     
 def eliminarAdministrador(usuario):
     try:
         # Eliminar el administrador
-        cursor.execute("DELETE FROM administradores WHERE usuario = ?", (usuario,))
+        cursor.execute("DELETE FROM administradores WHERE usuario = %s", (usuario))
         conn.commit()
 
         # Verificar si realmente se eliminó alguna fila
@@ -45,24 +49,24 @@ def eliminarAdministrador(usuario):
 
 def validarAdministrador (usuarioIngresado,contraseñaIngresado):
     
-    cursor.execute('SELECT * FROM administradores WHERE usuario=? AND contraseña=?', (usuarioIngresado,contraseñaIngresado))
+    cursor.execute('SELECT * FROM administradores WHERE usuario=%s AND contraseña=%s', (usuarioIngresado,contraseñaIngresado))
     resultado =cursor.fetchone()
     
     if resultado:    
         #Retornamos un mensaje de acceso Concedido
-        return 'Acceso concedido.'
+        return True
         #Llamamos la funcion para cambiar el GUI
     else:
-        return 'Usuario y contraseña incorrectos.'       
-
-#Funcion para añadir un plato al menu
+        return False           
+    
+ #Funcion para añadir un plato al menu
 def añadir_plato(self, plato):
     pass
-    
+
 #Funcion para modificar un plato
 def modificar_plato(self, plato):
     pass
-    
+
 #Funcion para eliminar un plato
 def eliminar_plato(self, plato):
     pass
@@ -70,7 +74,7 @@ def eliminar_plato(self, plato):
 #Funcion para añadir un ingrediente
 def añadir_ingrediente(self, ingredientes):
     pass
-    
+
 #Funcion para modificar un ingrediente
 def modificar_ingrediente(self, ingredientes):
     pass
